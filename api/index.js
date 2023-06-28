9f2b3bc3e264d07a82ebeac7f6e69e0783c76a2b
Param Kesarwani <kparam6162@gmail.com>
2023-06-29 00:52:57 +0530

    Edited Cors

--- api/index.js
+++ api/index.js
@@ -34,12 +34,14 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(cors())
 
-app.use(cors({
-    origin:"http://localhost:5001",
-    methods:"GET,POST,PUT,DELETE",
-    credentials:true
-}));
+// app.use(cors({
+//     origin:"http://localhost:5001",
+//     methods:"GET,POST,PUT,DELETE",
+//     credentials:true
+// }));
+
 
 app.use('/api/auth', authRoute)
 app.use('/api/movie', movieRoute)
